391ecb491080918411feecb7f3ff2426d16041f5
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2022-12-03 23:53:01 +0530

    Update circular_ll.cpp

--- circular_ll.cpp
+++ circular_ll.cpp
@@ -1,3 +1,13 @@
+// Applications of circular linked lists:
+//     Multiplayer games use this to give each player a chance to play.
+//     A circular linked list can be used to organize multiple running applications on an operating system. 
+//     These applications are iterated over by the OS.
+
+// Why circular linked list?
+//     A node always points to another node, so NULL assignment is not necessary.
+//     Any node can be set as the starting point.
+//     Nodes are traversed quickly from the first to the last.
+
 #include <iostream>
 using namespace std;
 
