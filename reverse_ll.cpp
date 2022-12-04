202e77a9de80336cbc833a414f89b21d53e4b0a5
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2022-12-04 23:56:03 +0530

    Update reverse_ll.cpp

--- reverse_ll.cpp
+++ reverse_ll.cpp
@@ -1,1 +1,80 @@
+#include <iostream>
+using namespace std;
 
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp -> next = n;
+}
+
+node* reverse(node* &head){
+
+    node* prev = NULL;
+    node* current = head;
+    node* nxt;
+
+    while(current != NULL){
+
+        nxt = current -> next;
+        current->next = prev;
+        prev = current;
+        current = nxt;
+
+    }
+    head = prev;
+    return prev;
+}
+
+void print(node* head){
+
+    node* temp = head;
+
+    while(temp != NULL){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+
+}
+
+int main(){
+
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    // insertAtTail(head, 5);
+    print(head);
+
+    node* newhead = reverse(head);
+    print(newhead);
+
+    print(head);
+
+    
+
+    return 0;
+}
