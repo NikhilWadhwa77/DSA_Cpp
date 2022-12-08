fd3b36e7c9ff6fbcc8b677c5f187024270320915
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2022-12-08 23:51:49 +0530

    Update LinkedList_insertion.cpp

--- linkedList/LinkedList_insertion.cpp
+++ linkedList/LinkedList_insertion.cpp
@@ -1,1 +1,90 @@
+#include <iostream>
+using namespace std;
+// insertion
 
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int value){
+        data = value;
+        next = NULL;
+    }
+
+};
+
+void insertAtHead(node* &head, int val){
+
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    n->next = head;
+    head = n;
+}
+
+void insertAtMiddle(node* &head, int val, int val1, int val2){
+
+    node* n = new node(val);
+    node* temp = head;
+
+    while(temp->data != val1){
+        temp = temp->next;
+    }
+     n->next = temp->next;
+    temp->next = n;
+
+
+}
+
+void insertAtTail(node* &head, int val){
+
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    
+    temp->next = n;
+}
+
+void display(node* head){
+
+    if(head == NULL){
+        cout << "head -> NULL" << endl;
+        return;
+    }
+    node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+
+
+int main(){
+
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    display(head);
+    insertAtHead(head,0);
+    display(head);
+    insertAtMiddle(head,69,2,3);    
+    display(head);
+
+    return 0;
+}
